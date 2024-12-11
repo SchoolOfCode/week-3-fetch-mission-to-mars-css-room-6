58267caf92c78127b251cfa694d6af0201f36a3c
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-11 13:43:47 +0000

    Trial to find code

--- launch-sequence.js
+++ launch-sequence.js
@@ -4,7 +4,8 @@ async function submitCode() {
   try {
     // Your investigation code should go here
     // Leave your lines of code in when you find something out, so that you can always come back to it and see how you got there
-    const response = await fetch("/api/logs");
+    const response = await fetch("/api/"
+);
     const data = await response.json();
     console.log(data);
   } catch (error) {
